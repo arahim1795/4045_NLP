ce4e7febf53ba8cefe73d0acee931bf71c9b7b86
Amartur Rahim <amarturrahim1795@gmail.com>
2019-10-02 15:28:31 +0800

    Completed data preparation

---
+++ Data/prepare_data.py
@@ -0,0 +1,41 @@
+import re
+import json
+
+def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"[Ww]on\'t", "will not", phrase)
+    phrase = re.sub(r"[Cc]an\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    # phrase = re.sub(r"\'s", " is", phrase)
+    # phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+def split_review(phrase):
+    phrases = phrase.split('\n')
+    phrases = list(filter(None, phrases))
+    return phrases
+
+def merge_phrases(phrases):
+    phrases = " ".join(phrases)
+    return re.sub(r"  ", " ", phrases)
+
+
+i = 1
+review_dic = {}
+data_file = 'reviewSamples20.json'
+with open(data_file, 'r') as json_file:
+    for line in json_file:
+        review = json.loads(line)
+        review['text'] = merge_phrases(split_review(decontracted(review['text'])))
+        review_dic[i] = review
+        i = i + 1
+
+with open('processed_data.json', 'w') as json_file:
+    json.dump(review_dic, json_file)
