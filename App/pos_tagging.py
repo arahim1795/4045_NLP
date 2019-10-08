5d01c896135609266cb50777d8616e16cb51b87c
akyulas <bolshack_fire@hotmail.com>
2019-10-08 15:59:37 +0800

    Added pos tagger for 5 random sentences with stanford nlp

--- App/pos_tagging.py
+++ App/pos_tagging.py
@@ -18,13 +18,13 @@ with open(data_file, 'r') as json_file:
 
 random_5_sentences = random.sample(sentences_list, 5)
 
-doc = nlp("#MAGA")
-parsed_text = {}
-for sent in doc.sentences:
-    for wrd in sent.words:
-        parsed_text[wrd.text] = wrd.upos
-
-print(parsed_text)
+for sentence in random_5_sentences:
+    doc = nlp(sentence)
+    parsed_text = {}
+    for sent in doc.sentences:
+        for wrd in sent.words:
+            parsed_text[wrd.text] = wrd.upos
+    print(parsed_text)
 
 
 # for sentence in random_5_sentences:
