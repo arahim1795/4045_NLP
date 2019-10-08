c654ead5e3f10b85566624545ac789e46e3e471a
akyulas <bolshack_fire@hotmail.com>
2019-10-08 15:49:33 +0800

    Added Stanford POS tagging

--- App/pos_tagging.py
+++ App/pos_tagging.py
@@ -1,9 +1,12 @@
 import json
 import spacy
 import random
+import stanfordnlp
 
 data_file = '../Data/test.json'
-nlp = spacy.load('en_core_web_sm')
+# nlp = spacy.load('en_core_web_sm')
+
+nlp = stanfordnlp.Pipeline(processors = "tokenize,mwt,lemma,pos")
 
 sentences_list = []
 
@@ -14,7 +17,22 @@ with open(data_file, 'r') as json_file:
         sentences_list.append(sentences)
 
 random_5_sentences = random.sample(sentences_list, 5)
-for sentence in random_5_sentences:
-    doc = nlp(sentence)
-    for token in doc:
-        print(token.text, token.tag_)
+
+doc = nlp("#MAGA")
+parsed_text = {}
+for sent in doc.sentences:
+    for wrd in sent.words:
+        parsed_text[wrd.text] = wrd.upos
+
+print(parsed_text)
+
+
+# for sentence in random_5_sentences:
+#     doc = nlp(sentence)
+#     parsed_text = {}
+#     for sent in doc.sentences:
+#         for wrd in sent.words:
+#             parsed_text[wrd.text] = wrd.upos
+#     print(parsed_text)
+    # for token in doc:
+    #     print(token.text, token.tag_)
